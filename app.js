f52e23b7f68cf7f58400f63b487adf93d214b816
oaksas <aman.getnet2@gmail.com>
2022-03-15 10:24:29 -1100

    Setting working environment

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const require("express.js")
+const express = require("express");
+const app = express();
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerJSDocs = YAML.load("./api.yaml");
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerJSDocs));
+app.listen(4000, () => console.log("UP & RUNNING"));
